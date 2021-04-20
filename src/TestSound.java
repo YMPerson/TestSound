83081b74570d91d2cf0a3174a106a88d34bf0572
YMPerson <82401541+YMPerson@users.noreply.github.com>
2021-04-21 00:34:20 +0400

    Хочу вот так

--- src/TestSound.java
+++ src/TestSound.java
@@ -1,10 +1,10 @@
 public class TestSound {
     public static void main(String[] args) {
 
-        System.out.println("HELLO");
-        System.out.println("WORLD");
+
         for (int i = 1; i < 10 ; i++) {
-            System.out.println("Akram " + i);
+            System.out.println("HELLO");
+            System.out.println("WORLD");
 
         }
         Sound.playSound("src/res/clk.wav").join();
