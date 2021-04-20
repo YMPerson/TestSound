152b1f276f6744a17eccaff9e0fef5e106dd2ce2
YMPerson <82401541+YMPerson@users.noreply.github.com>
2021-04-21 00:15:29 +0400

    Добавили WORLD!

--- src/TestSound.java
+++ src/TestSound.java
@@ -2,6 +2,7 @@ public class TestSound {
     public static void main(String[] args) {
 
         System.out.println("HELLO");
+        System.out.println("WORLD");
         Sound.playSound("src/res/clk.wav").join();
 
 }
